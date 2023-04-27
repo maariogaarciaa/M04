be43b6eb014818b3d3e2a628eb8c772fcbe54ae9
yui <mario.garcia.7e6@itb.at>
2023-04-27 12:22:35 +0200

    youu

---
+++ A1 - Javascript/hello.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="hello.js" </script>
+</head>
+<body>
+    <h1> MGP PGM </h1>
+</body>
+</html>
